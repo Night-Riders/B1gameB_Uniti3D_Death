6ddfcf76e9f41f20b182a02d8be99242b16e7e2c
ClementProject <cousseauclement@gmail.com>
2019-02-07 17:55:06 +0100

    Debut du code pour le bonus de temps

---
+++ Assets/Script/BonusObject.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public
+public class BonusObject : MonoBehaviour {
+
+        void OnCollisionEnter(Collision col)
+        {
+            if (col.gameObject.name == "Bonus Time")
+            {
+                Destroy(col.gameObject);
+            }
+        }
+    
+}
