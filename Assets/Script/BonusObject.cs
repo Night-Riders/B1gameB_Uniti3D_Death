b3d9ccada4b04bb4f18d1f31712cbe0ee4f70ece
squeletteur <antoinemulliez@orange.fr>
2019-03-06 08:28:44 +0100

    *.*

---
+++ Assets/Script/BonusObject.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BonusObject : MonoBehaviour {
+
+        void OnCollisionEnter(Collision col)
+        {
+            if (col.gameObject.name == "Bonus Time")
+            {
+                Destroy(col.gameObject);
+            }
+        }
+    
+}
---
+++ Assets/Script/Death/Scr_PlayerMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scr_PlayerMovement : MonoBehaviour {
+
+    public float movementSpeed;
+
+    private GameObject child;
+
+	// Use this for initialization
+	void Start () {
+        child = transform.GetChild(0).gameObject;
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        Vector3 mousePos = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+        mousePos.z = transform.position.z + 5;
+        transform.Translate(new Vector3((mousePos.x -0.5f) *5, 0, 1*movementSpeed)*Time.deltaTime);
+        Debug.Log(mousePos);
+    }
+}
