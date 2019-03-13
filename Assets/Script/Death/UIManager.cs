fb5c5b13b35a1dfeb9a6bdc6171bb5c7da8ba5f1
ClementProject <cousseauclement@gmail.com>
2019-03-13 08:43:43 +0100

    UI Score + debut du code

--- Assets/Script/Death/UIManager.cs
+++ Assets/Script/Death/UIManager.cs
@@ -8,6 +8,9 @@ public class UIManager : MonoBehaviour {
     public float timerValue = 30f;
     private float currentTimerValue = 0f;
     public Text timeText;
+    private float Score = 0f;
+    private float currentScoreValue = 0f;
+    public Text scoreText;
     private void Awake()
     {
         if(Singleton !=null)
@@ -22,12 +25,17 @@ public class UIManager : MonoBehaviour {
     // Use this for initialization
     void Start () {
         currentTimerValue = timerValue;
+        currentScoreValue = Score;
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
         currentTimerValue -= Time.deltaTime;
         timeText.text = currentTimerValue.ToString("Time : "+ " 0.0 "+"s");
+        currentScoreValue += Time.deltaTime;
+        scoreText.text = currentScoreValue.ToString("Score :");
+
 	}
     public void AddTenSeconds()
     {
