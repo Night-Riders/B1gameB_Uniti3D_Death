892060437840a797827978e355193ff6d890aa18
squeletteur <antoinemulliez@orange.fr>
2019-03-14 14:26:53 +0100

    incrémentation du score en fonction du Z du joueur

--- Assets/Script/Death/UI_Manager/UIManager.cs
+++ Assets/Script/Death/UI_Manager/UIManager.cs
@@ -5,16 +5,20 @@ using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour {
     public static UIManager Singleton;
+
     public float timerValue = 30f;
     private float currentTimerValue = 0f;
     public Text timeText;
-    public float Score = 0.1f;
+
     private float currentScoreValue = 0f;
     public Text scoreText;
+
+    public GameObject joueur;
+
     private void Awake()
     {
-        if(Singleton !=null)
-            {
+        if (Singleton != null)
+        {
             Destroy(gameObject);
         }
         else
@@ -23,22 +27,27 @@ public class UIManager : MonoBehaviour {
         }
     }
     // Use this for initialization
-    void Start () {
+    void Start() {
         currentTimerValue = timerValue;
-        currentScoreValue = Score;
-
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        currentTimerValue -= Time.deltaTime;
-        timeText.text = currentTimerValue.ToString("Time : "+ " 0.0 "+"s");
-        currentScoreValue = Time.deltaTime;
-        scoreText.text = currentScoreValue.ToString("Score :");
-
-	}
-    public void AddTenSeconds()
-    {
-        currentTimerValue += 10f;
     }
+
+    // Update is called once per frame
+    void Update() {
+        currentTimerValue -= Time.deltaTime;
+        timeText.text = currentTimerValue.ToString("Time : " + " 0.0 " + "s");
+        IncrementScore();
+        scoreText.text = (currentScoreValue/2).ToString("Score :" + " 0");
+    }
+    public void AddTenSeconds()
+        {
+            currentTimerValue += 10f;
+        }
+
+    public void IncrementScore()
+        {
+        if (joueur.transform.position.z > currentScoreValue)
+            {
+                currentScoreValue = joueur.transform.position.z;
+            }
+        }
 }
