28dd380b48e8a71221268e6bbad2cf81ba015437
squeletteur <antoinemulliez@orange.fr>
2019-03-18 11:30:53 +0100

    spawner d'ennemis à des emplacements aléatoires qui avance avec le joueur en étant toujours en dehors de la caméra.

---
+++ Assets/Script/Death/Enemy/SpawnerManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SpawnerManager : MonoBehaviour {
+
+    public GameObject ennemi;
+    public Transform positionsParent;
+    public float spawnInterval;
+    private float survivedTime = 0f;
+
+    // Use this for initialization
+    void Start()
+    {
+        InvokeRepeating("GenerateEnemy", 0f, spawnInterval);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+    void GenerateEnemy()
+    {
+        Instantiate(ennemi, positionsParent.GetChild(Random.Range(0, positionsParent.childCount)).position, Quaternion.identity);
+
+    }
+}
