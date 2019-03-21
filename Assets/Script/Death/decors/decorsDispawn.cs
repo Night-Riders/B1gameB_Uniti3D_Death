4dfda2e423e00b4d86b44b2c0f5286973b595ffe
squeletteur <antoinemulliez@orange.fr>
2019-03-21 14:15:26 +0100

    push de linterface, les décors et autres, mais bug sur le spawn de décors

---
+++ Assets/Script/Death/decors/decorsDispawn.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class decorsDispawn : MonoBehaviour {
+
+    private GameObject player;
+    private Transform playerTransform;
+
+    // Use this for initialization
+    void Start () {
+        player = GameObject.Find("Player");
+        playerTransform = player.transform;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if ((playerTransform.position.z + 50 ) > transform.position.x)
+        {
+            Destroy(gameObject);
+        }
+	}
+}
